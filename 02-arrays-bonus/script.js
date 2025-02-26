41c49aa24e730e91faa84135999ce41c517496da
Marco-Rocco <marcorocco28@icloud.com>
2025-02-26 17:03:07 +0100

    fix: ';'

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -28,4 +28,4 @@ const isFabioPresent = teachers.includes("Fabio");
 console.log(isFabioPresent);
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.toString();
-console.log(teachersString)
+console.log(teachersString);
